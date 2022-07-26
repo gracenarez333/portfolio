00d85680349d809fb9a2bc6261d09900990cc040
gracenarez333 <gracemnarez@gmail.com>
2022-07-25 18:46:53 -0700

    preps for deployement

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,17 +1,17 @@
 // built in link from next.js
-import Link from 'next/link'
+// import Link from 'next/link'
 
-export default function Navbar() {
-    return (
-        <>
-            <nav>
-                <Link href='/'>
-                    Home
-                </Link>
-                <Link href='/about'>
-                    About Me
-                </Link>
-            </nav>
-        </>
-    )
-}
+// export default function Navbar() {
+//     return (
+//         <>
+//             <nav>
+//                 <Link href='/'>
+//                     Home
+//                 </Link>
+//                 <Link href='/about'>
+//                     About Me
+//                 </Link>
+//             </nav>
+//         </>
+//     )
+// }
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Navbar from '../components/navbar'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Component {...pageProps} />
     </>
   )
