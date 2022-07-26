5a5ba9a29785c2c9b0272178041b6f353afc5a08
gracenarez333 <gracemnarez@gmail.com>
2022-07-25 18:56:29 -0700

    preps for deployement

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/globals.css'
-import Navbar from '../components/navbar'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Navbar /> */}
       <Component {...pageProps} />
     </>
   )
